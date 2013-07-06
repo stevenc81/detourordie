2967dc3c22c2077fcf180e7f9644586a4f4b60db
Steven Cheng <stevenc81@gmail.com>
2013-07-06 18:09:15 +0800

    - allow core for page serving

--- mobileweb/app.js
+++ mobileweb/app.js
@@ -24,6 +24,19 @@ if ('development' == app.get('env')) {
 if ('production' == app.get('env')) {
 // app.set('db uri', 'n.n.n.n/prod');
 }
+/*
+ * CORS Support in Node.js web app written with Express
+ */
+
+// http://stackoverflow.com/questions/7067966/how-to-allow-cors-in-express-nodejs
+app.all('/*', function (req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", 'Content-Type, X-Requested-With');
+        res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+        next();
+});
+// handle OPTIONS requests from the browser
+app.options("*", function (req,res,next) { res.send(200); });
 
 app.listen(app.get('port'), function() {
     console.log("Express server listening on port " + app.get('port') + ' with mode ' + process.env.ENV_NODE);
